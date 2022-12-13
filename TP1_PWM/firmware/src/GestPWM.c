ee175b2190e9fa31f337ac073b97b57b17a74799
ETMLNET\maeclerc <a@a.ch>
2022-12-13 15:12:22 +0100

    Fonctions GPWM_GetSettings et GPWM_DispSettings finies (Remise de GestPWM.c et GestPWM.h au format etml-es)

--- TP1_PWM/firmware/src/GestPWM.c
+++ TP1_PWM/firmware/src/GestPWM.c
@@ -1,132 +1,50 @@
-/* ************************************************************************** */
-
-#include <stdint.h>
-
-#include "Mc32DriverAdc.h"
-
-/** Descriptive File Name
-
-  @Company
-    Company Name
-
-  @File Name
-    filename.c
-
-  @Summary
-    Brief description of the file.
-
-  @Description
-    Describe the purpose of this file.
- */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: Included Files                                                    */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/* This section lists the other files that are included in this file.
- */
-
-/* TODO:  Include other files here if needed. */
+/*--------------------------------------------------------*/
+// GestPWM.c
+/*--------------------------------------------------------*/
+//	Description :	Gestion des PWM 
+//			        pour TP1 2022-2023
+//
+//	Auteur 		: 	M.Clerc
+//                  M.Santos
+//
+//	Version		:	V1.1
+//	Compilateur	:	XC32 V1.42 + Harmony 1.08
+//
+/*--------------------------------------------------------*/
 
 
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: File Scope or Global Data                                         */
-/* ************************************************************************** */
-/* ************************************************************************** */
 
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
+#include "GestPWM.h"
+#include "bsp.h"
 
-/* ************************************************************************** */
-/** Descriptive Data Item Name
+S_pwmSettings PWMData;      // pour les settings
 
-  @Summary
-    Brief one-line summary of the data item.
-    
-  @Description
-    Full description, explaining the purpose and usage of data item.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-    
-  @Remarks
-    Any additional remarks
- */
-int global_data;
-
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-// Section: Local Functions                                                   */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-/* ************************************************************************** */
-
-/** 
-  @Function
-    int ExampleLocalFunctionName ( int param1, int param2 ) 
-
-  @Summary
-    Brief one-line description of the function.
-
-  @Description
-    Full description, explaining the purpose and usage of the function.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-
-  @Precondition
-    List and describe any required preconditions. If there are no preconditions,
-    enter "None."
-
-  @Parameters
-    @param param1 Describe the first parameter to the function.
-    
-    @param param2 Describe the second parameter to the function.
-
-  @Returns
-    List (if feasible) and describe the return values of the function.
-    <ul>
-      <li>1   Indicates an error occurred
-      <li>0   Indicates an error did not occur
-    </ul>
-
-  @Remarks
-    Describe any special behavior not described above.
-    <p>
-    Any additional remarks.
-
-  @Example
-    @code
-    if(ExampleFunctionName(1, 2) == 0)
-    {
-        return 3;
-    }
- */
-
-void GPWM_GetSettings()
+void GPWM_Initialize(S_pwmSettings *pData)
 {
-    /* Local variables */
+   // Init les data 
+    
+   // Init état du pont en H
+    BSP_EnableHbrige();
+    
+   // lance les timers et OC
+    DRV_TMR0_Start();
+    DRV_TMR1_Start();
+    DRV_TMR2_Start();
+    DRV_TMR3_Start();
+    
+}
+
+// Obtention vitesse et angle (mise a jour des 4 champs de la structure)
+void GPWM_GetSettings(S_pwmSettings *pData)	
+{
+    /* Variables locales */
     static uint8_t Moving_Average_Memory = 0;
     static uint8_t i;
     static uint8_t Moving_Average_Chan0[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     static uint8_t Moving_Average_Chan1[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     static uint8_t Average_Chan0 = 0;
     static uint8_t Average_Chan1 = 0;
+    static uint8_t Convert_Speed = 0;
     static S_ADCResults Results;
     
     // Lecture de l'ADC
@@ -152,36 +70,39 @@ void GPWM_GetSettings()
     
     Average_Chan0 = Average_Chan0 / 10;
     Average_Chan1 = Average_Chan1 / 10;
+    
+    // Conversion des valeurs
+    Convert_Speed = Average_Chan0 / 5.166;
+    PWMData.SpeedSetting = Convert_Speed - 99;
+    PWMData.absSpeed = abs(PWMData.SpeedSetting);
+    PWMData.AngleSetting = Average_Chan1 / 5.683;
+    PWMData.absAngle = abs(PWMData.AngleSetting);
 }
 
 
-/* ************************************************************************** */
-/* ************************************************************************** */
-// Section: Interface Functions                                               */
-/* ************************************************************************** */
-/* ************************************************************************** */
+// Affichage des information en exploitant la structure
+void GPWM_DispSettings(S_pwmSettings *pData)
+{
+    printf_lcd("Tp1 PWM 2022-2023");
+    lcd_gotoxy(1, 2);
+    printf_lcd("SpeedSetting %3d", PWMData.SpeedSetting);
+    lcd_gotoxy(1, 3);
+    printf_lcd("absSpeed      %2d", PWMData.absSpeed);
+    lcd_gotoxy(1, 4);
+    printf_lcd("Angle        %3d", PWMData.AngleSetting);
+    lcd_bl_on();
+}
 
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
+// Execution PWM et gestion moteur à partir des info dans structure
+void GPWM_ExecPWM(S_pwmSettings *pData)
+{
+    
+}
 
-// *****************************************************************************
-
-/** 
-  @Function
-    int ExampleInterfaceFunctionName ( int param1, int param2 ) 
-
-  @Summary
-    Brief one-line description of the function.
-
-  @Remarks
-    Refer to the example_file.h interface header for function usage details.
- */
-int ExampleInterfaceFunction(int param1, int param2) {
-    return 0;
+// Execution PWM software
+void GPWM_ExecPWMSoft(S_pwmSettings *pData)
+{
+    
 }
 
 
-/* *****************************************************************************
- End of File
- */
--- TP1_PWM/firmware/src/GestPWM.h
+++ TP1_PWM/firmware/src/GestPWM.h
@@ -1,179 +1,43 @@
-/* ************************************************************************** */
-/** Descriptive File Name
+#ifndef GestPWM_H
+#define GestPWM_H
+/*--------------------------------------------------------*/
+// GestPWM.h
+/*--------------------------------------------------------*/
+//	Description :	Gestion des PWM 
+//			        pour TP1 2016-2017
+//
+//	Auteur 		: 	C. HUBER
+//
+//	Version		:	V1.1
+//	Compilateur	:	XC32 V1.42 + Harmony 1.08
+//
+/*--------------------------------------------------------*/
 
-  @Company
-    Company Name
-
-  @File Name
-    filename.h
-
-  @Summary
-    Brief description of the file.
-
-  @Description
-    Describe the purpose of this file.
- */
-/* ************************************************************************** */
-
-#ifndef _EXAMPLE_FILE_NAME_H    /* Guard against multiple inclusion */
-#define _EXAMPLE_FILE_NAME_H
+#include <stdint.h>
 
 
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: Included Files                                                    */
-/* ************************************************************************** */
-/* ************************************************************************** */
 
-/* This section lists the other files that are included in this file.
- */
-
-/* TODO:  Include other files here if needed. */
+/*--------------------------------------------------------*/
+// Définition des fonctions prototypes
+/*--------------------------------------------------------*/
 
 
-/* Provide C++ Compatibility */
-#ifdef __cplusplus
-extern "C" {
+typedef struct {
+    uint8_t absSpeed;    // vitesse 0 à 99
+    uint8_t absAngle;    // Angle  0 à 180
+    int8_t SpeedSetting; // consigne vitesse -99 à +99
+    int8_t AngleSetting; // consigne angle  -90 à +90
+} S_pwmSettings;
+
+extern S_pwmSettings PWMData; 
+
+void GPWM_Initialize(S_pwmSettings *pData);
+
+// Ces 3 fonctions ont pour paramètre un pointeur sur la structure S_pwmSettings.
+void GPWM_GetSettings(S_pwmSettings *pData);	// Obtention vitesse et angle
+void GPWM_DispSettings(S_pwmSettings *pData);	// Affichage
+void GPWM_ExecPWM(S_pwmSettings *pData);		// Execution PWM et gestion moteur.
+void GPWM_ExecPWMSoft(S_pwmSettings *pData);	// Execution PWM software.
+
+
 #endif
-
-
-    /* ************************************************************************** */
-    /* ************************************************************************** */
-    /* Section: Constants                                                         */
-    /* ************************************************************************** */
-    /* ************************************************************************** */
-
-    /*  A brief description of a section can be given directly below the section
-        banner.
-     */
-
-
-    /* ************************************************************************** */
-    /** Descriptive Constant Name
-
-      @Summary
-        Brief one-line summary of the constant.
-    
-      @Description
-        Full description, explaining the purpose and usage of the constant.
-        <p>
-        Additional description in consecutive paragraphs separated by HTML 
-        paragraph breaks, as necessary.
-        <p>
-        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-    
-      @Remarks
-        Any additional remarks
-     */
-#define EXAMPLE_CONSTANT 0
-
-
-    // *****************************************************************************
-    // *****************************************************************************
-    // Section: Data Types
-    // *****************************************************************************
-    // *****************************************************************************
-
-    /*  A brief description of a section can be given directly below the section
-        banner.
-     */
-
-
-    // *****************************************************************************
-
-    /** Descriptive Data Type Name
-
-      @Summary
-        Brief one-line summary of the data type.
-    
-      @Description
-        Full description, explaining the purpose and usage of the data type.
-        <p>
-        Additional description in consecutive paragraphs separated by HTML 
-        paragraph breaks, as necessary.
-        <p>
-        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-
-      @Remarks
-        Any additional remarks
-        <p>
-        Describe enumeration elements and structure and union members above each 
-        element or member.
-     */
-    typedef struct _example_struct_t {
-        /* Describe structure member. */
-        int some_number;
-
-        /* Describe structure member. */
-        bool some_flag;
-
-    } example_struct_t;
-
-
-    // *****************************************************************************
-    // *****************************************************************************
-    // Section: Interface Functions
-    // *****************************************************************************
-    // *****************************************************************************
-
-    /*  A brief description of a section can be given directly below the section
-        banner.
-     */
-
-    // *****************************************************************************
-    /**
-      @Function
-        int ExampleFunctionName ( int param1, int param2 ) 
-
-      @Summary
-        Brief one-line description of the function.
-
-      @Description
-        Full description, explaining the purpose and usage of the function.
-        <p>
-        Additional description in consecutive paragraphs separated by HTML 
-        paragraph breaks, as necessary.
-        <p>
-        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-
-      @Precondition
-        List and describe any required preconditions. If there are no preconditions,
-        enter "None."
-
-      @Parameters
-        @param param1 Describe the first parameter to the function.
-    
-        @param param2 Describe the second parameter to the function.
-
-      @Returns
-        List (if feasible) and describe the return values of the function.
-        <ul>
-          <li>1   Indicates an error occurred
-          <li>0   Indicates an error did not occur
-        </ul>
-
-      @Remarks
-        Describe any special behavior not described above.
-        <p>
-        Any additional remarks.
-
-      @Example
-        @code
-        if(ExampleFunctionName(1, 2) == 0)
-        {
-            return 3;
-        }
-     */
-    int ExampleFunction(int param1, int param2);
-
-
-    /* Provide C++ Compatibility */
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* _EXAMPLE_FILE_NAME_H */
-
-/* *****************************************************************************
- End of File
- */
